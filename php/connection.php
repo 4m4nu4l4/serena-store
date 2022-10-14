8e4e95f19f326565f9f10c2931a3301f6159d053
eduardabenevenutti77 <mariaeduardabenevenutti77@gmail.com>
2022-10-14 09:01:07 -0300

    connection

--- php/connection.php
+++ php/connection.php
@@ -1,12 +1,12 @@
 ?php 
 
-    define('HOST', '000webhost'); 
+   define('HOST', '000webhost'); 
 
-    define('DB', 'id19648590_serena'); 
+   define('DB', 'id19648590_serena'); 
 
     define('USER', 'id19648590_serena_store'); 
 
-    define("PASS", 'spZxF^%)Rs6kp]rr'); 
+   define("PASS", 'spZxF^%)Rs6kp]rr'); 
 
   
 
@@ -14,7 +14,7 @@
 
         $pdo = new PDO('mysql:host='.HOST.';dbname='.DB, USER, PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8")); 
 
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    { $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
     } catch (Exception $e){ 
 
