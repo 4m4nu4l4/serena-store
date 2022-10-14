a3eb5d191b02cc0a466c4692e23801543a665159
eduardabenevenutti77 <mariaeduardabenevenutti77@gmail.com>
2022-10-14 08:38:27 -0300

    connection

---
+++ php/connection.php
@@ -0,0 +1,27 @@
+?php 
+
+    define('HOST', 'localhost'); 
+
+    define('DB', 'id19648590_serena'); 
+
+    define('USER', 'id19648590_serena_store'); 
+
+    define("PASS", 'spZxF^%)Rs6kp]rr'); 
+
+  
+
+    try{ 
+
+        $pdo = new PDO('mysql:host='.HOST.';dbname='.DB, USER, PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8")); 
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+    } catch (Exception $e){ 
+
+        echo '<h1> Erro ao conectar com o banco !!' . DB.'</h1>'; 
+
+    } 
+
+     
+
+?> 
